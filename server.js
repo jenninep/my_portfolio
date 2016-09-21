b9e3fe8acc9cf1db9591a4f8f56b668604bb99b1
Jennine Punzone <jennine@Jennine-Punzones-MacBook-Pro.local>
2016-09-20 21:14:08 -0400

    added node server

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require ('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+// var ParseServer = require('parse-server').ParseServer;
+
+//Sets up the the express app
+var app = express();
+
+//Where all of the post information is being pushed to
+//setting up the port that the server will be listening on
+var PORT = process.env.PORT || 3000;
+
+//Sets up the express app to handle parsing
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ 
+	limit: '50mb',
+	extended: true, 
+	parameterLimit:50000}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+
+//displaying data from the home.html file
+app.get('/', function(req,res){
+	res.sendfile(path.join(__dirname, 'public/index.html'));
+});
+
+//lets the server recognize the js files
+app.use(express.static('public'));
+
+//starts the server with the listening queue
+app.listen(PORT, function(){
+	console.log("Listening on port", PORT);
+});
